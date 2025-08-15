5a629c0d64ad47c96c2030fb953aff8a938abf91
Suyash Patil <suyashpatil4500@gmail.com>
2025-08-15 19:22:25 +0530

    added tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        "primary-dull": "var(--color-primary-dull)",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"], // optional if you want Tailwind classes
+      },
+    },
+  },
+  plugins: [],
+};
