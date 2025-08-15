22fec99262901b11691b2770cf5de1b282cf0263
Suyash Patil <suyashpatil4500@gmail.com>
2025-08-16 00:34:24 +0530

    added fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import MainBanner from "../components/MainBanner";
 import Categories from "../components/Categories";
 import BestSeller from "../components/BestSeller";
 import BottomBanner from "../components/BottomBanner";
-import NewsLetter from "../components/NewsLetter";
 import Testimonials from "../components/Testimonials";
 
 const Home = () => {
